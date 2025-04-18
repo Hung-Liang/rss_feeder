bdaf61630148e86374cc452aa1876db752561cbc
Hung <zxjonathanzx@gmail.com>
2025-04-18 23:34:22 +0800

    Revert "remove unuse code"
    This reverts commit 51891adea6fd02f68a236d56874c000d073fe0e6.

--- main.py
+++ main.py
@@ -62,9 +62,11 @@ def start_tmux_session(rss_url, webhook_url, history_file):
 
 
 if __name__ == "__main__":
-    for config in configs:
-        webhook_url = config["web_hook"]
-        rss_url = config["rss_url"]
-        history_file = config["history_file"]
+    while True:
+        for config in configs:
+            webhook_url = config["web_hook"]
+            rss_url = config["rss_url"]
+            history_file = config["history_file"]
 
-        start_tmux_session(rss_url, webhook_url, history_file)
+            start_tmux_session(rss_url, webhook_url, history_file)
+        time.sleep(3600)
